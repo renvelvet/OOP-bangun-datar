4afa9197c06ec9a657d38b83a8b705d83e52d394
Resha Puspita <reshapuspita734@gmail.com>
2020-06-22 09:50:30 +0700

    edit method name

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ class BangunDatar {
     this.keliling = keliling;
   }
 
-  persegi(s) {
+  hiyungPersegi(s) {
     this.luas = s * s;
     this.keliling = 4 * s;
     return `Luas = ${this.luas}, Keliling = ${this.keliling}`;
   }
 
-  persegiPanjang(p, l) {
+  hitungPersegiPanjang(p, l) {
     this.luas = p * l;
     this.keliling = 2 * p + 2 * l;
     return `Luas = ${this.luas}, Keliling = ${this.keliling}`;
   }
 
-  segitigaSiku2(a, t) {
+  hitungSegitigaSikuSiku(a, t) {
     this.luas = (1 / 2) * a * t;
     this.keliling = a + t + sqrt(a * a + t * t);
     return `Luas = ${this.luas}, Keliling = ${this.keliling}`;
   }
 
-  lingkarang(r) {
+  hitungLingkaran(r) {
     this.luas = Math.PI * r * r;
     this.keliling = 2 * Math.PI * r;
     return `Luas = ${this.luas}, Keliling = ${this.keliling}`;
